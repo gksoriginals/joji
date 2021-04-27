e30856f2010fb21fdf2e97db50872f87501966ee
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2021-04-27 16:41:25 +0530

    Create jojify.py

---
+++ joji/jojify.py
@@ -0,0 +1,45 @@
+import os
+import json
+import spacy
+nlp = spacy.load("en_core_web_md")
+
+emoji_dict_path = "/data/emoji_dict.json"
+emoji_dict = open(emoji_dict_path)
+emoji_dict = json.load(emoji_dict)
+
+class Jojify(object):
+  """
+  class containing methods to map text to emoji
+  """  
+  emoji_dict = emoji_dict
+  
+  @classmethod
+  def _simple_check(cls, text):
+    if text in cls.emoji_dict:
+      return cls.emoji_dict.get(text)
+    return None
+  
+  @staticmethod
+  def _similarity_match(word1, word2):
+      word1 = nlp(word1)
+      word2 = nlp(word2)
+      return word1.similarity(word2)
+    
+  @classmethod  
+  def _emoji_match(cls, text):
+    max_score = 0 # set threshold value instead
+    max_emoji = None
+    for emoji_name in cls.emoji_dict:
+      score = cls._similarity_match(text, emoji_name)
+      if score > max_score:
+        max_emoji = cls.emoji_dict.get(emoji_name)
+   return max_emoji, max_score
+
+  @classmethod
+  def predict(cls, text):
+    emoji = cls._simple_check(text)
+    emoji = cls._emoji_match if not emoji else emoji
+    return emoji if emoji else else text
+  
+ 
+  
