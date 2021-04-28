ebbd1bc55e3fad242d325d2e025c541f9eada8b2
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2021-04-28 22:32:42 +0530

    Improve  code

--- joji/jojify.py
+++ joji/jojify.py
@@ -29,57 +29,6 @@ class Jojify(object):
   emoji_dict = emoji_dict
 
   @staticmethod
-  def generate_unicode(emoj):
-    return f'U+{ord(emoj):X}' if len(emoj) == 1 else emoj
-  
-  @classmethod
-  def _simple_check(cls, text):
-    if text in cls.emoji_dict:
-      emoji_ = emoji.emojize(cls.emoji_dict.get(text).get("short_name"))
-      return (
-        emoji_,
-        cls.generate_unicode(emoji_),
-        1
-      )
-    return None
-  
-  @staticmethod
-  def _similarity_match(word1, word2):
-    word1 = nlp(word1)
-    return word1.similarity(word2)
-
-  @classmethod
-  def _similarity_match_1(cls, word1, word2):
-    word1 = nlp(word1)
-    return cls.cosine_similarity_numba(word1.vector, word2.vector)
-
-    
-  @classmethod  
-  def _context_similarity_check(cls, text):
-    max_score = 0 # set threshold value instead
-    max_emoji = None
-    for emoji_name in cls.emoji_dict:
-      score = cls._similarity_match(text, emoji_dict.get(emoji_name).get("vector"))
-      if score > max_score:
-        max_emoji = emoji.emojize(cls.emoji_dict.get(emoji_name).get("short_name"))
-        max_score = score
-    max_unicode = cls.generate_unicode(max_emoji)
-    return max_emoji, max_unicode, max_score
-
-
-  @staticmethod
-  def preprocess(text):
-    text = text.lower()
-    return text
-
-  @classmethod
-  def predict(cls, text):
-    text = cls.preprocess(text)
-    emoji = cls._simple_check(text)
-    emoji = cls._context_similarity_check(text) if not emoji else emoji
-    return emoji if emoji else text
-  
-  @staticmethod
   @jit(nopython=True)
   def cosine_similarity_numba(u:np.ndarray, v:np.ndarray):
     assert(u.shape[0] == v.shape[0])
@@ -94,4 +43,68 @@ class Jojify(object):
     if uu!=0 and vv!=0:
         cos_theta = uv/np.sqrt(uu*vv)
     return cos_theta
+
+  @classmethod
+  def _similarity_match_1(cls, word1, word2):
+    word1 = nlp(word1)
+    return cls.cosine_similarity_numba(word1.vector, word2.vector)
+
+  @staticmethod
+  def generate_unicode(emoj):
+    return f'U+{ord(emoj):X}' if len(emoj) == 1 else None
+  
+  @staticmethod
+  def _simple_check(text, emoji_dict):
+    emoji_data, score = None, 0.0
+    if text in emoji_dict:
+      emoji_data = emoji_dict.get(text)
+    return emoji_data, score
+  
+  @staticmethod
+  def _similarity_match(word1, nlpfied2):
+    word1 = nlp(word1) 
+    return word1.similarity(nlpfied2)
+
+  @staticmethod
+  def get_vector(emoji_data):
+    return emoji_data.get("vector")
+    
+  @classmethod  
+  def _context_similarity_check(cls, text, emoji_dict):
+    max_score = 0 # set threshold value instead
+    max_emoji_data = None
+    for emoji_name in emoji_dict:
+      emoji_data = emoji_dict.get(emoji_name)
+      vector = cls.get_vector(emoji_data)
+      score = cls._similarity_match(text, vector)
+      if score > max_score:
+        max_score = score
+        max_emoji_data = emoji_data
+    return max_emoji_data, max_score
+
+  @staticmethod
+  def get_short_name(emoji_data):
+    return emoji_data.get("short_name")
+
+  @classmethod
+  def get_emoji_and_unicode(cls, emoji_data):
+    short_name = cls.get_short_name(emoji_data)
+    max_emoji = emoji.emojize(short_name)
+    max_unicode = cls.generate_unicode(max_emoji)
+    return max_emoji, max_unicode
+
+  @staticmethod
+  def preprocess(text):
+    text = text.lower()
+    return text
+
+  @classmethod
+  def predict(cls, text):
+    text = cls.preprocess(text)
+    emoji_data, score = cls._simple_check(text, cls.emoji_dict)
+    if not emoji_data:
+      emoji_data, score = cls._context_similarity_check(text, cls.emoji_dict)
+    max_emoji, max_unicode = cls.get_emoji_and_unicode(emoji_data)
+    return (max_emoji, max_unicode, score) if score else text
+  
   
