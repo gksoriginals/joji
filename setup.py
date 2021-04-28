54926c03a952c5774bda07c746315c151bda3144
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2021-04-28 15:38:37 +0530

    Update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name="joji",
-    version="1.0.6",
+    version="1.0.7",
     description="convert a word to corresponding emoji",
     long_description=README,
     long_description_content_type="text/markdown",
